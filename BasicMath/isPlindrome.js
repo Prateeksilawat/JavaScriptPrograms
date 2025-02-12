894f3e88bb8641bb048a868577b6823c5488f5f4
Prateeksilawat <142071957+Prateeksilawat@users.noreply.github.com>
2025-02-12 23:24:49 +0530

    Basic Math Question

---
+++ BasicMath/isPlindrome.js
@@ -0,0 +1,18 @@
+function isPalindrome(str) {
+    let start = 0;
+    let end = str.length - 1;
+
+    while (start < end) {
+        if (str[start] !== str[end]) {
+            return false;  // Return false if characters don't match
+        }
+        start++;  // Move towards the center from the left
+        end--;    // Move towards the center from the right
+    }
+
+    return true;  // Return true if all characters match
+}
+
+const str = "madam";
+const result = isPalindrome(str);
+console.log(result);  // Output: true
