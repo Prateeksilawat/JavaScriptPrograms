22176e24f92ba79066d6a74ed009499d003ce86b
Prateeksilawat <142071957+Prateeksilawat@users.noreply.github.com>
2025-09-03 08:41:27 +0530

    02/sep/2025

--- BasicMath/PrimeOrNot.js
+++ BasicMath/PrimeOrNot.js
@@ -1,28 +1,45 @@
+// const NumberPrime = (num) => {
 
+//     let count = 0
+//     for(let i = 1 ;i<= Math.sqrt(num) ; i++){
 
-const NumberPrime = (num) => {
+//         if(num%i === 0){
+//             count++
 
-    let count = 0
-    for(let i = 1 ;i<= Math.sqrt(num) ; i++){
+//             if(num/i != i){
+//                 count++
+//             }
+//         }
+//     }
 
-        if(num%i === 0){
-            count++
+//     if(count === 2){
+//         console.log('Number is Prime');
 
-            if(num/i != i){
-                count++
-            }
-        }
+//     }else{
+//         console.log('Number is not Prime');
+
+//     }
+// }
+
+// NumberPrime(83)
+// NumberPrime(73)
+// NumberPrime(1)
+
+function isPrime(num) {
+  let count = 0;
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      count++;
+      if (num / i != i) {
+        count++;
+      }
     }
-
-    if(count === 2){
-        console.log('Number is Prime');
-        
-    }else{
-        console.log('Number is not Prime');
-        
-    }
+  }
+  if (count === 2) {
+    console.log("Is a Prime number");
+  } else {
+    console.log("Not a prime number");
+  }
 }
 
-NumberPrime(83)
-NumberPrime(73)
-NumberPrime(5)
+isPrime(101);
--- BasicMath/factorial.js
+++ BasicMath/factorial.js
@@ -1,14 +1,23 @@
 
-const Factorial = (n) => {
+// const Factorial = (n) => {
  
-    if( n == 0 ) {
-        return 1
+//     if( n == 0 ) {
+//         return 1
+//     }
+
+//     return n * Factorial(n-1)
+
+// }
+
+// let N = 5
+
+// console.log(`Factorial ${N} numbers using recursion is:`, Factorial(N))
+
+function Factorial(num){
+    let result = 1
+    for(i=1;i<=num;i++){
+        result = result * i
     }
-
-    return n * Factorial(n-1)
-
+    return result
 }
-
-let N = 5
-
-console.log(`Factorial ${N} numbers using recursion is:`, Factorial(N))
+console.log(Factorial(0))
