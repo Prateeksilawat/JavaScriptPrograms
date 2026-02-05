b153ceef64b239b0ca376ee59ae2e50e663400d8
Prateeksilawat <142071957+Prateeksilawat@users.noreply.github.com>
2026-02-06 00:27:39 +0530

    add optimal solution

--- BasicProblems/NumberIsPrime.js
+++ BasicProblems/NumberIsPrime.js
@@ -1,15 +1,39 @@
 //Problem 1: Check if a Number is Prime
 
+//Brute Force Approach
+//Time complexity O(n)
 function CheckNumberIsPrime (num){
-  if(num === 0 || num === 1) return false
+  if(num === 0 || num === 1) {
+    console.log(`Number ${num} is not Prime`)
+    return
+  }
     for(let i = 2 ;i< num -1 ; i++){
 
         if(num % i === 0){
-            return false
+            console.log(`Number ${num} is not Prime`)
+            return 
         }
     }
-    return true
-    
+ 
+console.log( `Number ${num} is Prime`)    
 }
+CheckNumberIsPrime(28)
 
-console.log(CheckNumberIsPrime(5));
+//Optimal Approch
+//Time complexity O(√n)
+function CheckNumberIsPrime (num){
+  if(num === 0 || num === 1) {
+    console.log(`Number ${num} is not Prime`)
+    return
+  }
+    for(let i = 2 ;i*i <= num  ; i++){
+
+        if(num % i === 0){
+            console.log(`Number ${num} is not Prime`)
+            return 
+        }
+    }
+ 
+console.log( `Number ${num} is Prime`)    
+}
+CheckNumberIsPrime(28)
